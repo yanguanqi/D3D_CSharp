dec97ecd0b1df69c8c6ce910d33671b88eb51f49
yanguanqi <yanguanqi@YANGUANQI-PC>
2017-06-22 13:58:55 +0800

    D3D_CSharp_Base_0304

--- Test/F0304.cs
+++ Test/F0304.cs
@@ -15,6 +15,7 @@ namespace Test
         private bool pause = false;
         private Device mDevice;
         CustomVertex.TransformedColored[] verts = null, verts1 = null;
+        VertexBuffer vertexBuffer = null;
         public F0304()
         {
             InitializeComponent();
@@ -54,8 +55,36 @@ namespace Test
         }
         private void OnCreateDevice(object sender, EventArgs e)
         {
-
+            Device dev = (Device)sender;
+            vertexBuffer = new VertexBuffer(typeof(CustomVertex.TransformedColored), 3, dev, 0, CustomVertex.TransformedColored.Format, Pool.Default);
+            vertexBuffer.Created += onCreateVertexBuffer;
+           
+            this.onCreateVertexBuffer(vertexBuffer, null);
         }
+        private void onCreateVertexBuffer(object sender,EventArgs e)
+        {
+            CustomVertex.TransformedColored[] verts = (CustomVertex.TransformedColored[])vertexBuffer.Lock(0, 0);
+            verts[0].X = 150;
+            verts[0].Y = 50;
+            verts[0].Z = 0.5f;
+            verts[0].Rhw = 1;
+            verts[0].Color = Color.Aqua.ToArgb();
+
+            verts[1].X = 250;
+            verts[1].Y = 250;
+            verts[1].Z = 0.5f;
+            verts[1].Rhw = 1;
+            verts[1].Color = Color.Brown.ToArgb();
+
+            verts[2].X = 50;
+            verts[2].Y = 250;
+            verts[2].Z = 0.5f;
+            verts[2].Rhw = 1;
+            verts[2].Color = Color.LightPink.ToArgb();
+            vertexBuffer.Unlock();
+        }
+     
+
         private void OnResetDevice(object sender, EventArgs e)
         {
 
@@ -75,7 +104,9 @@ namespace Test
             mDevice.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.Blue, 1, 0);
             mDevice.BeginScene();
             //Todo: Input your code
-
+            mDevice.SetStreamSource(0, vertexBuffer, 0);
+            mDevice.VertexFormat = CustomVertex.TransformedColored.Format;
+            mDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 1);
             mDevice.EndScene();
             mDevice.Present();
 
